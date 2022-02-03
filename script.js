3e6c75faf6b6629966272f0b2f987621697289a3
Rafael Longo <rafaelongo45@gmail.com>
2022-02-03 04:02:53 -0300

    consertei o css. cartas podem ser viradas. gifs estão aleatórios

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let lista = [];
 let qtdCartas = null;
+let nomeGifs = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
 let listaCartas = [];
+
+nomeGifs.sort(comparador);
 
 function checaQuantidade(){
     for (let i = 4; i<=14;i+=2){
@@ -20,17 +23,28 @@ function checaQuantidade(){
 
 function adicionaCartas(){
     const main = document.querySelector("main");
-    for (let i = 0; i < qtdCartas; i++){
-        listaCartas.push('<div class = "container"><img src="./imagens/front.png" alt="parrot card"></div>');
+    for (let i = 0; i < qtdCartas/2; i++){
+        listaCartas.push(`<section onclick="clicarCarta(this)"><div class="container front"><img src="./imagens/front.png" alt="parrot card"></div><div class="back container nao-aparece"><img src="./imagens/${nomeGifs[i]}.gif" alt="parrot card"></div></section>`);
+        listaCartas.push(`<section onclick="clicarCarta(this)"><div class="container front"><img src="./imagens/front.png" alt="parrot card"></div><div class="back container nao-aparece"><img src="./imagens/${nomeGifs[i]}.gif" alt="parrot card"></div></section>`);
+    }
+
+    listaCartas.sort(comparador)
+    for (let i = 0; i < listaCartas.length; i++){
         main.innerHTML = main.innerHTML + listaCartas[i];
     }
 }
-//<div class = "container"><img src="./imagens/front.png" alt="parrot card"></div>
+
+function clicarCarta(elemento){
+    let divBack = elemento.querySelector('.back');  
+    let divFront = elemento.querySelector('.front');  
+    divBack.classList.remove('nao-aparece');
+    divFront.classList.add('nao-aparece');
+}
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-listaCartas.sort(comparador); 
+
 checaQuantidade();
 adicionaCartas();
